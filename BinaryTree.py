5a6870b5223a5406f756a676868e05955943546d
Yuri <112394310+ltsmeyuri@users.noreply.github.com>
2023-09-02 18:50:31 -0300

    Add files via upload

---
+++ BinaryTree.py
@@ -0,0 +1,24 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.leftchild = None
+        self.rightchild = None
+        
+    def insertLeft(self, value):
+        if self.leftchild == None:
+            self.leftchild = BinaryTree(value)
+        else:
+            newnode = BinaryTree(value)
+            newnode.leftchild = self.leftchild
+            self.leftchild = newnode
+            
+    def insertRight(self, value):
+        if self.rightchild == None:
+            self.rightchild = BinaryTree(value)
+        else:
+            newnode = BinaryTree(value)
+            newnode.rightchild = self.rightchild
+            self.rightchild = newnode
+            
+tree1 = BinaryTree(1)
+tree1.insertLeft(2)
